cf84dafa649815488b9ce897901eebf88c5514af
Husnamehween <150599046+Husnamehween@users.noreply.github.com>
2024-12-10 13:16:35 +0530

    utils code discussion part2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-kmm
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app= express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN
+}))
+
+export{app}
